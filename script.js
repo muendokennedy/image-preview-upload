b3bd847bd465d000f34cc16b21080e280cc04aed
muendokennedy <kennedymunyao999@gmail.com>
2023-09-16 08:30:15 +0300

    Implemented the drag and drop file upload feature using javascript

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const dragArea = document.querySelector('.drag-area');
 const dragText = document.querySelector('.header');
 
+let file;
+
 
 // When the file is inside the drag area
 
@@ -20,4 +22,25 @@ dragArea.addEventListener('dragleave', () => {
 // When the file is dropped in the drag area
 dragArea.addEventListener('drop', (e) => {
   e.preventDefault();
+  file = e.dataTransfer.files[0];
+
+  let fileType = file.type;
+
+  let validExtensions = ['image/jpeg', 'image/jpg', 'image/png'];
+
+  if(validExtensions.includes(fileType)){
+    let fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      let fileURL = fileReader.result;
+      let imgTag = `<img src="${fileURL}" alt="">`;
+      dragArea.innerHTML = imgTag;
+    }
+    fileReader.readAsDataURL(file);
+  } else {
+    alert('This file is not an image');
+    dragArea.classList.remove('active');
+  }
+
+  console.log(fileType);
 });

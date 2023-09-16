5817e914e793801e00452daf531f34a1eeaaf1ad
muendokennedy <kennedymunyao999@gmail.com>
2023-09-16 08:50:26 +0300

    Removed the active class on the drag-area on file display

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ button.onclick = () => {
   input.click();
 }
 
+// When browse
+input.addEventListener('change', function(){
+  file = this.files[0];
+  dragArea.classList.add('active');
+  displayFile();
+});
+
 
 // When the file is inside the drag area
 
@@ -28,9 +35,14 @@ dragArea.addEventListener('dragleave', () => {
 
 // When the file is dropped in the drag area
 dragArea.addEventListener('drop', (e) => {
+
   e.preventDefault();
   file = e.dataTransfer.files[0];
+  displayFile();
 
+});
+
+function displayFile(){
   let fileType = file.type;
 
   let validExtensions = ['image/jpeg', 'image/jpg', 'image/png'];
@@ -44,10 +56,8 @@ dragArea.addEventListener('drop', (e) => {
       dragArea.innerHTML = imgTag;
     }
     fileReader.readAsDataURL(file);
+    dragArea.classList.remove('active');
   } else {
     alert('This file is not an image');
-    dragArea.classList.remove('active');
   }
-
-  console.log(fileType);
-});
+}
